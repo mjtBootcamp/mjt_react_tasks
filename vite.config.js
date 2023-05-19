259a13627ca5d21575781004837a029c58d315c5
mjtBootcamp <bootcamp.javascript.fullstack@gmail.com>
2023-05-18 20:04:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mjt_react_tasks/'
+  base: 'https://mjtBootcamp.gitlab.io/mjt_react_tasks/'
 })
